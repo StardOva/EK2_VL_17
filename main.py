4c4c7a0dc00570a77310e52d008b3fcd77f2ad24
Moritz Reichelt <41913675+StardOva@users.noreply.github.com>
2023-05-11 17:51:38 +0200

    add support for neutral element

--- main.py
+++ main.py
@@ -28,24 +28,45 @@ a = float(input("Parameter a: "))
 b = float(input("Parameter b: "))
 p = float(input("Primzahl p: "))
 
+P_unendlich = False
+Q_unendlich = False
+
 print("Hinweis: Punkt muss die Form x,y haben")
-P = input("Punkt P: ").replace(" ", "").split(",")
-Q = input("Punkt Q: ").replace(" ", "").split(",")
+P = input("Punkt P: ").replace(" ", "")
+if "," in P:
+    P = P.split(",")
+elif P == "O":
+    P_unendlich = True
 
-# in integers umwandeln
-P[0] = float(P[0])
-P[1] = float(P[1])
-Q[0] = float(Q[0])
-Q[1] = float(Q[1])
+Q = input("Punkt Q: ").replace(" ", "")
+if "," in Q:
+    Q = Q.split(",")
+elif Q == "O":
+    Q_unendlich = True
 
-# TODO Parameterprüfung: Länge von P und Q muss 2 sein
-
-# Fallunterscheidung, ob eine Punktaddition oder eine Punktverdopplung stattfinden muss
-# Es wird eine Punktverdopplung durchgeführt, wenn beide Punkte die selben Koordinaten haben.
-
-if P[0] == Q[0] and P[1] == Q[1]:
-    print(f"Punkt P {P} und Q {Q} sind gleich. Es wird eine Punktverdopplung durchgeführt.")
+# Behandlung vom Punkt in der Unendlichkeit
+if P_unendlich and not Q_unendlich:
+    print("P dient als neutrales Element, Ergebnis ist Q = ", Q)
+elif not P_unendlich and Q_unendlich:
+    print("Q dient als neutrales Element, Ergebnis ist P = ", P)
+elif P_unendlich and Q_unendlich:
+    print("Beide Elemente sind das neutrale Element, Ergebnis ist O")
 else:
-    print(f"Punkt P {P} und Q {Q} sind nicht gleich. Es wird eine Punktaddition durchgeführt.")
-    R = addPoint(P, Q, p)
-    print(R)
+
+    # in integers umwandeln
+    P[0] = float(P[0])
+    P[1] = float(P[1])
+    Q[0] = float(Q[0])
+    Q[1] = float(Q[1])
+
+    # TODO Parameterprüfung: Länge von P und Q muss 2 sein
+
+    # Fallunterscheidung, ob eine Punktaddition oder eine Punktverdopplung stattfinden muss
+    # Es wird eine Punktverdopplung durchgeführt, wenn beide Punkte die selben Koordinaten haben.
+
+    if P[0] == Q[0] and P[1] == Q[1]:
+        print(f"Punkt P {P} und Q {Q} sind gleich. Es wird eine Punktverdopplung durchgeführt.")
+    else:
+        print(f"Punkt P {P} und Q {Q} sind nicht gleich. Es wird eine Punktaddition durchgeführt.")
+        R = addPoint(P, Q, p)
+        print(R)
