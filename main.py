fix overflow error

main.py
@@ -1,10 +1,22 @@
 import math
-
+import numpy as np
 
 def addPoint(P, Q, p):
-    s = (int((Q[1] - P[1]) / (Q[0] - P[0]))) % p
-    x_3 = (math.pow(s, 2) - P[0] - Q[0]) % p
-    y_3 = (s * (P[0] - x_3) - P[1]) % p
+    s_zaehler = (Q[1] - P[1]) % p
+    print("s_zaehler", s_zaehler)
+
+    s_nenner = (Q[0] - P[0]) % p
+    print("s_nenner", s_nenner)
+
+    s_inverse = np.power(s_nenner, p-2)
+    print("s_inverse", s_inverse)
+
+    s = (s_zaehler * s_inverse) % p
+
+    print("s =", s)
+
+    x_3 = float((math.pow(s, 2) - P[0] - Q[0]) % p)
+    y_3 = float((s * (P[0] - x_3) - P[1]) % p)
 
     return [x_3, y_3]
 
@@ -13,21 +25,20 @@ def doublePoint(P, Q):
     print("Todo")
 
 
-a = int(input("Parameter a: "))
-b = int(input("Parameter b: "))
-p = int(input("Primzahl p: "))
+a = float(input("Parameter a: "))
+b = float(input("Parameter b: "))
+p = float(input("Primzahl p: "))
 
 print("Hinweis: Punkt muss die Form x,y haben")
 P = input("Punkt P: ").replace(" ", "").split(",")
 Q = input("Punkt Q: ").replace(" ", "").split(",")
 
 # in integers umwandeln
-P[0] = int(P[0])
-P[1] = int(P[1])
-Q[0] = int(Q[0])
-Q[1] = int(Q[1])
+P[0] = float(P[0])
+P[1] = float(P[1])
+Q[0] = float(Q[0])
+Q[1] = float(Q[1])
 
-# Todo entscheiden ob Addition oder Verdopplung
 # TODO Parameterprüfung: Länge von P und Q muss 2 sein
 
 # Fallunterscheidung, ob eine Punktaddition oder eine Punktverdopplung stattfinden muss
@@ -39,4 +50,3 @@ else:
     print(f"Punkt P {P} und Q {Q} sind nicht gleich. Es wird eine Punktaddition durchgeführt.")
     R = addPoint(P, Q, p)
     print(R)
-