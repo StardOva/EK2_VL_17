03e1f05622ab6cf9a7c7dce6b02616d7e516f79d
Moritz Reichelt <41913675+StardOva@users.noreply.github.com>
2023-05-11 16:37:45 +0200

    Grundgerüst

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello World")
+def addPoint(P, Q):
+    print("Todo")
+
+def doublePoint(P, Q):
+    print("Todo")
+
+
+a = int(input("Parameter a: "))
+b = int(input("Parameter b: "))
+
+print("Hinweis: Punkt muss die Form x,y haben")
+P = input("Punkt P: ").replace(" ", "").split(",")
+Q = input("Punkt Q: ").replace(" ", "").split(",")
+
+
+
+
